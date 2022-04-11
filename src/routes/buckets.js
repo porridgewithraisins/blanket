1dc2968287c9bf5301a57c79d59d13b3d0fff06a
Siddharth Singh <simsid66@gmail.com>
2022-04-12 01:29:17 +0530

    bucket creation and deletion code

--- src/routes/buckets.js
+++ src/routes/buckets.js
@@ -1,6 +1,21 @@
-function createBucket(req, res, next) {
-    const { name, description } = req.body;
-    const { project_id } = req.params;
+const { Bucket } = require('../models');
 
-    
+function createBucket(req, res, next) {
+    const { name } = req.body;
+    const { projectId } = req.params;
+
+    const bucket = await Bucket.create({
+        name, projectId
+    });
+}
+
+function deleteBucket(req, res, next) {
+    const { name } = req.body;
+    const { projectId } = req.params;
+
+    const bucket = await Bucket.findOne({
+        name, projectId
+    })
+
+    bucket.destroy();
 }
