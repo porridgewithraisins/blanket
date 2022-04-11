7d33136b9e74515b4bfae333a60f0c704e8c35db
porridgewithraisins <santhanambr2002@gmail.com>
2022-04-12 00:57:55 +0530

    shfudhfdfdsd

---
+++ src/models.js
@@ -0,0 +1,39 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "database.sqlite",
+});
+
+const Project = sequelize.define("Project", {
+    name: {
+        type: String,
+        allowNull: false,
+    },
+    description: { type: String },
+    crust_info: { type: String },
+});
+
+const Bucket = sequelize.define("Bucket", {
+    name: {
+        type: String,
+        allowNull: false,
+    },
+});
+
+const File = sequelize.define("File", {
+    cid: {
+        type: String,
+        allowNull: false,
+    },
+    filename: {
+        type: String,
+        allowNull: false,
+    },
+});
+
+Project.hasMany(Bucket);
+Bucket.belongsTo(Project);
+
+Bucket.hasMany(File);
+File.belongsTo(Bucket);
