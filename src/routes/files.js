c1bf65ed15f13afa8a4692ddc8e269afd2a921c7
hemang <hemangahujaeotw@gmail.com>
2022-04-12 06:52:59 +0530

    ui + api = magik

--- src/routes/files.js
+++ src/routes/files.js
@@ -3,59 +3,62 @@ const ipfsClient = require("ipfs-http-client");
 const fs = require("fs/promises");
 const { prisma } = require("../db");
 module.exports = {
-    async addFile(req, res, next) {
-        const file = req.files.file;
-        const { fileName } = req.body;
-        const { bucket_id } = req.params;
+  async addFile(req, res, next) {
+    console.log(req.file);
+    if (!req.file) {
+      res.status(400).send({ msg: "No files were uploaded." });
+      return;
+    }
+    const file = req.file;
+    const { fileName } = req.body;
+    const { bucket_id } = req.params;
 
-        const filePath = "files/" + fileName;
+    const fileDetail = await addFileAuth(file.path);
 
-        await file.mv(filePath);
+    await fs.unlink(file.path);
 
-        const fileDetail = await addFileAuth(filePath);
+    const cid = fileDetail.cid.toString().slice();
 
-        await fs.unlink(filePath);
-
-        const cid = fileDetail.cid.toString().slice();
-
-        await prisma.file.create({ data: { cid, name: fileName, bucketId: Number(bucket_id) } });
-
-        res.json({ cid });
-    },
+    await prisma.file.create({
+      data: { cid, name: fileName, bucketId: Number(bucket_id) },
+    });
+    console.log(cid);
+    res.json({ cid });
+  },
 };
 
 async function addFileAuth(file_path) {
-    const pair = ethers.Wallet.createRandom();
-    console.log(pair);
-    const sig = await pair.signMessage(pair.address);
-    console.log(sig);
-    const authHeaderRaw = `eth-${pair.address}:${sig}`;
-    console.log(authHeaderRaw);
-    const authHeader = Buffer.from(authHeaderRaw).toString("base64");
-    console.log(authHeader);
-    const ipfsW3GW = "https://crustipfs.xyz";
+  const pair = ethers.Wallet.createRandom();
+  console.log(pair);
+  const sig = await pair.signMessage(pair.address);
+  console.log(sig);
+  const authHeaderRaw = `eth-${pair.address}:${sig}`;
+  console.log(authHeaderRaw);
+  const authHeader = Buffer.from(authHeaderRaw).toString("base64");
+  console.log(authHeader);
+  const ipfsW3GW = "https://crustipfs.xyz";
 
-    const fileBuffer = await fs.readFile(file_path);
+  const fileBuffer = await fs.readFile(file_path);
 
-    const ipfs = ipfsClient.create({
-        url: `${ipfsW3GW}/api/v0`,
-        headers: {
-            authorization: `Basic ${authHeader}`,
-        },
-    });
+  const ipfs = ipfsClient.create({
+    url: `${ipfsW3GW}/api/v0`,
+    headers: {
+      authorization: `Basic ${authHeader}`,
+    },
+  });
 
-    const { cid } = await ipfs.add({
-        path: file_path,
-        content: fileBuffer,
-    });
-    console.log(cid);
+  const { cid } = await ipfs.add({
+    path: file_path,
+    content: fileBuffer,
+  });
+  console.log(cid);
 
-    const fileStat = await ipfs.files.stat("/ipfs/" + cid);
-    console.log("FILESTAT");
-    console.log(fileStat);
+  const fileStat = await ipfs.files.stat("/ipfs/" + cid);
+  console.log("FILESTAT");
+  console.log(fileStat);
 
-    return {
-        cumulativeSize: fileStat.cumulativeSize,
-        cid: fileStat.cid,
-    };
+  return {
+    cumulativeSize: fileStat.cumulativeSize,
+    cid: fileStat.cid,
+  };
 }
