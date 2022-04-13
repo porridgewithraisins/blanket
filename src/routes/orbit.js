d12a7bc2be299242b15f9047673febb36a90029d
porridgewithraisins <santhanambr2002@gmail.com>
2022-04-13 07:26:48 +0530

    fix commonjs stuff

--- src/routes/orbit.js
+++ src/routes/orbit.js
@@ -6,33 +6,33 @@
  */
 const { repo } = require('../orbit');
 
-export const createDb = async (req, res, next) => {
+module.exports.createDb = async (req, res, next) => {
     const { project_id } = req.params;
     await repo.init(project_id);
     res.send({ msg: "OK" });
 }
-export const kvGet = (req, res, next) => {
+module.exports.kvGet = (req, res, next) => {
     const db = repo.getDb(req.params.project_id);
     const { key } = req.params;
     res.send({
         val: db.get(key)
     });
 }
-export const kvPut = (req, res, next) => {
+module.exports.kvPut = (req, res, next) => {
     const db = repo.getDb(req.params.project_id);
     const { key, val } = req.body;
     await db.put(key, val);
     res.send({ msg: "OK" });
 
 }
-export const kvDel = (req, res, next) => {
+module.exports.kvDel = (req, res, next) => {
     const db = repo.getDb(req.params.project_id);
     const { key } = req.params;
     const mh = await db.del(key);
     res.send({ msg: "OK", mh });
 }
 
-export const kvAll = (req, res, next) => {
+module.exports.kvAll = (req, res, next) => {
     const db = repo.getDb(req.params.project_id);
     res.send({ all: db.all });
 }
