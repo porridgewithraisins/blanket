0797f7218c09e2f8e89632799553ad78219105f0
porridgewithraisins <santhanambr2002@gmail.com>
2022-04-13 08:44:10 +0530

    sdshdhsds

--- src/orbit.js
+++ src/orbit.js
@@ -1,16 +1,16 @@
-const IPFS = require("ipfs");
+const IPFS = require("ipfs-http-client");
 const OrbitDB = require("orbit-db");
 
-const ipfs = IPFS.create();
-const orbit = await OrbitDB.createInstance(ipfs);
+const ipfs = IPFS.create({url: 'http://localhost:5001'});
 
 class Repository {
     constructor() {
         this.dbs = {};
+        OrbitDB.createInstance(ipfs).then(orbit => (this.orbit = orbit));
     }
 
     async init(projectId) {
-        this.dbs[projectId] = await orbit.keyvalue(`${projectId}`);
+        this.dbs[projectId] = await this.orbit.keyvalue(`${projectId}`);
     }
 
     getDb(projectId) {
