2439982d858b24278b244a769d494af07ec64438
Siddharth Singh <simsid66@gmail.com>
2022-04-13 07:10:30 +0530

    fix ipfs creation

--- src/orbit.js
+++ src/orbit.js
@@ -1,6 +1,6 @@
 const IPFS = require('ipfs')
 const OrbitDB = require('orbit-db')
 
-const ipfs = new IPFS()
+const ipfs = ipfs.create()
 const orbitdb = await OrbitDB.createInstance(ipfs)
 module.exports = { orbitdb }
